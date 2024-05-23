1e3883a93c4346504912e845f6611c38c9007f09
Nikolai Kummer <nikolai@attabotics.com>
2024-05-22 21:37:08 -0600

    Organize folder structures by moving Arduino files to firmware directory

---
+++ firmware/azure-iot/arduino_secrets.h
@@ -0,0 +1,10 @@
+// Fill in  your WiFi networks SSID and password
+#define SECRET_SSID      "SHAW-AB09E0"
+#define SECRET_PASS      "2511670C5048"
+
+// Fill in the hostname of your Azure IoT Hub broker
+#define SECRET_BROKER    "eco-tracker-hub.azure-devices.net"
+
+// Fill in the device id
+#define SECRET_DEVICE_ID "eco-tracker-01"
+#define SECRET_DEVICE_PRIMARY_KEY "CzjkUSrLgDmMcCeR/wQF8vRF15BF4YFg+Vw7Ei4FvaI="

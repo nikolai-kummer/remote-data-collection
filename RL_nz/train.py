6e27fa25dd50d34297e3c989c9a7eab9f5849767
Nikolai Kummer <nikolai@attabotics.com>
2024-07-14 22:00:26 -0600

    Add plotting to training

--- RL_nz/train.py
+++ RL_nz/train.py
@@ -1,6 +1,9 @@
 from utils.plot import plot_results
 from environment.custom_env import CustomEnv
 
+import numpy as np
+import matplotlib.pyplot as plt
+
 def get_reward(action, current_power, message_count):
     reward = 0
 
@@ -34,6 +37,7 @@ def train(env: CustomEnv, agent, train_config):
         env.init_state(power_level=50, time=16, message_count=0)
         total_reward = 0
         power_list = []
+        action_list = []
         legitimate_messages_sent_this_episode = 0
 
         for day in range(env.N_DAYS):
@@ -45,6 +49,7 @@ def train(env: CustomEnv, agent, train_config):
                 current_power = env.get_power()
                 reward = get_reward(action, current_power, legitimate_messages)
                 power_list.append(current_power)
+                action_list.append(action)
                 legitimate_messages_sent_this_episode += legitimate_messages
                 agent.update_q_value(current_state, action, reward, env.encode_state())
                 total_reward += reward
@@ -54,11 +59,22 @@ def train(env: CustomEnv, agent, train_config):
         legitimate_messages_list.append(legitimate_messages_sent_this_episode)
         print(f'Episode {episode+1}: Total Reward = {total_reward}, Legitimate Messages Sent = {legitimate_messages_sent_this_episode}')
 
-        # if episode % 10000 == 0:
-        #     plot_results(power_list, episode)
+        # if episode % 1000 == 0:
+        # #     plot_results(power_list, "Power Distribution")
+        #     values, counts = np.unique(action_list, return_counts=True)
+        #     plt.figure(figsize=(8, 4))
+        #     plt.bar(env.action_list, counts, color='skyblue')
+        #     plt.show()
+                
 
     plot_results(reward_list, "Rewards")
     plot_results(legitimate_messages_list, "Legitimate Messages")
+    plot_results(power_list, "Legitimate Messages")
 
+    
+    values, counts = np.unique(action_list, return_counts=True)
+    plt.figure(figsize=(8, 4))
+    plt.bar(env.action_list, counts, color='skyblue')
+    print("Waiting for plot")
     save_results('results/reward_list.csv', reward_list)
     save_results('results/legitimate_messages_list.csv', legitimate_messages_list)
